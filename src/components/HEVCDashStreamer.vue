e9c43d886a79070a5970e7553e36d6c8102cfbbd
Christian Bach <christianbach1689@googlemail.com>
2020-02-02 16:03:17 +0100

    formatting

--- src/components/HEVCDashStreamer.vue
+++ src/components/HEVCDashStreamer.vue
@@ -3,24 +3,24 @@
     <mdb-row>
       <mdb-col md="6" v-bind:key="video.id" v-for="video in media">
         <div class="embed-responsive embed-responsive-16by9" style="margin: 10px;">
-            <div class="embed-responsive-item">
-           <div>
-                <video
-            id="video.id"
-            data-dashjs-player
-            controls
-            webkit-playsinline
-            allowfullscreen
-            v-bind:src="video.url"
-          ></video>
-          <div v-if="video.omnidirectional" class="embed-responsive-item">
-           <b-button class="float-right " :pressed.sync="is360Video">View in VR</b-button>
-           <div v-if="is360Video == true">
-              <VRDashStreamer v-bind:url="video.url"></VRDashStreamer>
+          <div class="embed-responsive-item">
+            <div>
+              <video
+                id="video.id"
+                data-dashjs-player
+                controls
+                webkit-playsinline
+                allowfullscreen
+                v-bind:src="video.url"
+              ></video>
+              <div v-if="video.omnidirectional" class="embed-responsive-item">
+                <b-button class="float-right" :pressed.sync="is360Video">View in VR</b-button>
+                <div v-if="is360Video == true">
+                  <VRDashStreamer v-bind:url="video.url"></VRDashStreamer>
+                </div>
+              </div>
             </div>
-          </div> 
-           </div>
-           </div>     
+          </div>
         </div>
       </mdb-col>
     </mdb-row>
@@ -43,9 +43,9 @@ export default {
   name: "AVCDashStreamer",
   props: {
     is360Video: {
-    default: false,
-  },
-    media : {}
+      default: false
+    },
+    media: {}
   },
   components: {
     mdbContainer,
@@ -53,16 +53,17 @@ export default {
     mdbCol,
     VRDashStreamer
   },
-   mounted() {
+  mounted() {
     this.is360Video = false;
-    console.log("HEVCDashStreamer is mounted with media: "+JSON.stringify(this.media));
-
-   },
+    console.log(
+      "HEVCDashStreamer is mounted with media: " + JSON.stringify(this.media)
+    );
+  },
   methods: {
-    startVideo : function(video){
-      console.log("video: ",video);
+    startVideo: function(video) {
+      console.log("video: ", video);
       if (video.omnidirectional) {
-       this.is360Video = true;
+        this.is360Video = true;
       } else {
         this.is360Video = false;
       }
